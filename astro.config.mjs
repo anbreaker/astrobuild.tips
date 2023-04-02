1f3e1109e3fca498d897edf3d14dcee4ac22078c
anbreaker <antunez19@gmail.com>
2023-04-02 13:58:50 +0200

    feat: other frameworks and other tips init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 
-import mdx from "@astrojs/mdx";
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: 'https://AstroBuild.tips',
+  integrations: [tailwind(), mdx(), react(), vue()],
 });
